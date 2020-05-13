d46251ba2888e74189dae9b13376d340e4a18353
Tester86 <tester86t@gmail.com>
2020-05-13 23:55:07 +0200

    Adding binary files and package manager

--- include/std.hpp
+++ include/std.hpp
@@ -27,26 +27,8 @@ vector<string> listdir(){
   return names;
 }
 
-int cd(char* path){
-
-    int exit_code = chdir(path);
-    
-    if(exit_code == -1){
-        printf("This directory does not exist: %s", path);
-    }
-}
-
 void install_pkg(string pkg_link, string filename){
 
-  string cmd = "./file_downloader " + pkg_link + " " + filename;
-  if(cd("$HOME/.omni_shell/bin") == 0){
-    system(cmd.c_str());
-  } else{
-    system("sudo mkdir $HOME/.omni_shell");
-    system("sudo mkdir $HOME/.omni_shell/bin");
-    system("cd $HOME/.omni_shell/bin");
-    system("curl -L -o file_downloader https://raw.githubusercontent.com/Tester86/omni_shell_hosted/master/bin/file_downloader || sudo wget -L -o file_downloader https://raw.githubusercontent.com/Tester86/omni_shell_hosted/master/bin/file_downloader");
-    system(cmd.c_str());
-  }
+  
 
 }
